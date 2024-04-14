25c45088f52d5463a88ab4fd84a67a4b5b70af84
KYESSS <113056636+KYESSS@users.noreply.github.com>
2024-04-14 18:07:27 +0100

    Coin Flip Simulator

---
+++ Coin Flip Simulator.py
@@ -0,0 +1,23 @@
+import random
+
+def flip_coin():
+    return random.choice(["Heads", "Tails"])
+
+print(flip_coin())
+
+for i in range(10):
+    print("flip "+ str(i+1) + ": "+flip_coin())
+
+    flips = int(input("Flips: "))
+
+    total_heads = 0
+    total_tails = 0
+
+    for i in range(flips):
+        if (flip_coin() == "heads"):
+            total_heads += 1
+        else:
+            total_tails += 1
+
+            print("Total Heads: " + str(total_heads))
+            print("Total Tails: " + str(total_tails))
